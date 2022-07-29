042eb2955cbb40eef154740ecde77cc9c5ad4f5e
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-07-29 16:43:17 +0530

    initial state of server

---
+++ src/com/socket/sample/Server.java
@@ -0,0 +1,14 @@
+/*
+ * Developed by - mGunawardhana
+ * Contact email - mrgunawardhana27368@gmail.com
+ * what's app - 071 - 9043372
+ */
+
+package com.socket.sample;
+
+public class Server implements Runnable{
+    @Override
+    public void run() {
+
+    }
+}
