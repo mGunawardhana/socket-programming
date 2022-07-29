8f7f7631addd3ca19665dea7521f80286212657c
mGunawardhana <mrgunawardhana27368@gmail.com>
2022-07-29 20:37:32 +0530

    setup main method to run the client

--- src/com/socket/sample/Client.java
+++ src/com/socket/sample/Client.java
@@ -73,5 +73,9 @@ public class Client implements Runnable {
         }
     }
 
+    public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
 }
 
